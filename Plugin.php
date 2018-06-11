89ca1fe9a66fce96fe5ec29031e11f5a710f494a
LucasZdv <lucaszdv@gmail.com>
2018-06-11 14:56:24 -0300

    Start project

---
+++ Plugin.php
@@ -0,0 +1,14 @@
+<?php namespace Awebsome\Finances;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    public function registerComponents()
+    {
+    }
+
+    public function registerSettings()
+    {
+    }
+}
